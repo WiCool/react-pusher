76248d98e1560e73385274df537b8ff76e07e9fe
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-07-07 14:56:10 -0600

    startin the authentication process with Auth0

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: "jason-conrad.auth0.com",
+  clientId: "8m11x8Qug5DKgE8HwPzmkmkaE5lpFR4J",
+  callbackUrl: 'http://localhost:3000/callback' //Callback URL set in the Auth0 dasbhoard.
+}
